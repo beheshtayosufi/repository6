e8a4249ea3b63d7e2bd43923cf9689de138572c9
beheshta <husna300@gmail.com>
2023-07-17 13:59:24 +0500

    my 6 reposit

---
+++ variables.py
@@ -0,0 +1,31 @@
+# addition
+def addition():
+   x =1000
+   y = 300000
+   print(x + y)
+
+addition()
+
+# multiply
+def multiply(): 
+   yearMonthes =12
+   daysofeveryMonth=30
+   print(yearMonthes * daysofeveryMonth)
+
+   multiply()
+
+   # subtraction
+def difference():
+      fatherage =62
+      sonage =23
+      print(fatherage - sonage,"agediffrence")
+
+difference()
+ 
+ # division
+def eachItemPrice(): 
+      totalprice =8675440
+      numberofiteams =45
+      print(totalprice / numberofiteams, "dollar")
+
+eachItemPrice() 
